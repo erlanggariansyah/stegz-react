c949de478e0d490121028fa49f23e9b91517dfad
Erlangga Riansyah <erlangga.riansyah@ifg-life.id>
2023-09-17 22:06:56 +0700

    Done logout

--- src/components/SignedInNavbar/SignedInNavbar.js
+++ src/components/SignedInNavbar/SignedInNavbar.js
@@ -2,17 +2,25 @@ import React from "react";
 import SignedInNavbarStyled from "../../styled/SignedInNavbarStyled";
 import ItemListStyled from "../../styled/ItemListStyled";
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const SignedInNavbar = () => {
     const navigator = useNavigate();
     const user = JSON.parse(sessionStorage.getItem('User'));
 
     const handleClickLogout = () => {
-        sessionStorage.removeItem('Authorization-Token');
-        sessionStorage.removeItem('User');
-        localStorage.removeItem('PKCEVerifier');
-
-        navigator("/");
+        axios.post("http://localhost:8080/api/v1/logout", {}, {
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + sessionStorage.getItem('Authorization-Token')
+            }
+        }).then(() => {
+            sessionStorage.removeItem('Authorization-Token');
+            sessionStorage.removeItem('User');
+            localStorage.removeItem('PKCEVerifier');
+    
+            navigator("/");
+        });
     }
 
     return (
